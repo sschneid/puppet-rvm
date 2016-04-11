53c564e56ff8a9b284dfa0bd03e9b9523cc8c3bb
Wayne Warren <waynr@sdf.org>
2016-04-11 10:23:37 -0700

    (MAINT) Remove now-unnecessary virtual resource declaration.

--- manifests/packages/redhat.pp
+++ manifests/packages/redhat.pp
@@ -2,12 +2,6 @@ class rvm::packages::redhat {
   $package_list = ['gcc-c++', 'patch', 'readline', 'readline-devel', 'zlib', 
                    'zlib-devel', 'libyaml-devel', 'libffi-devel', 
                    'openssl-devel', 'curl', 'make', 'bzip2']
-                   
-  # Virtualize Package list to prevent conflicts
-  @package { $package_list:
-    ensure => 'present',
-    tag    => 'rvm-packages',
-  }
   
   # Realize packages list. 
   each($package_list) |$package_name| {
